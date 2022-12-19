39b761805d32f5e8202a54c5b8b6bed2ca0ffdd4
prabhanshu1808 <prabhanshu1808@gmail.com>
2022-12-19 11:49:32 +0530

    "commit"

--- playerservice/src/test/java/com/playerService/PlayerSerivceApplicationTests.java
+++ playerservice/src/test/java/com/playerService/PlayerSerivceApplicationTests.java
@@ -1,13 +1,32 @@
 package com.playerService;
 
+
+import com.playerService.service.PlayerServiceImpl;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.support.AnnotationConfigContextLoader;
 
 @SpringBootTest
+@ContextConfiguration(classes = PlayerSerivceApplication.class)
 class PlayerSerivceApplicationTests {
 
+	private PlayerServiceImpl playerService;
+
 	@Test
-	void contextLoads() {
+	void ContextLoads(){
+
+	}
+
+	@Test
+	public void testWinning(){
+        String result = "You win!";
+		String first = "Rock";
+		String second = "Scissor";
+
+		assert(result).equals(playerService.evaluateMove(first , second));
+
+
 	}
 
 }
